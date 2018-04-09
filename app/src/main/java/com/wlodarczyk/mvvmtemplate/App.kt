6c11a2d2871a2447d2d3375dc424a52038a63383
damianoo <damian.wlodarczyk.dod@gmail.com>
2018-04-09 22:39:33 +0200

    Started with dagger

---
+++ app/src/main/java/com/wlodarczyk/mvvmtemplate/App.kt
@@ -0,0 +1,13 @@
+package com.wlodarczyk.mvvmtemplate
+
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+class App : Application(){
+//
+//    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+//        TODO("not implemented")
+//    }
+
+}
